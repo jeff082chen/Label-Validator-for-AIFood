6ef229c00f9ed5be5b1b950a0d9e1e90077d790f
Jeffrey Chen <jeff082chen@gmail.com>
2023-08-04 14:52:44 +0800

    08/04/2023 progress
    1. small changes on colors and layout

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 # Author: Jeffrey Chen
-# Last Modified: 08/03/2023
+# Last Modified: 08/04/2023
 import os, json, atexit, time, threading
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtGui import QColor
@@ -9,13 +9,29 @@ from PyQt5.QtGui import QPixmap
 div_size = 224
 pad = 3
 
+background_color_dark = "rgb(73, 131, 154)"
+background_color_light = "rgb(232, 243, 243)"
+text_color = "rgb(124, 166, 215)"
+
+button_style = f"""
+    QPushButton {{
+        background-color: {background_color_light};
+        color: {text_color};
+        border: 1px solid {text_color};
+    }}
+    QPushButton:hover {{
+        background-color: {text_color};
+        color: {background_color_light};
+    }}
+"""
+
 # Root window
 class Root(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Label Validator")
         self.setFixedSize(div_size * 2 + pad * 2, int(div_size * 1.25) + pad * 3)  # 454 x 283
-        self.setStyleSheet("background-color: white")
+        self.setStyleSheet(f"background-color: {background_color_dark};")
 
 # Backend logic
 class ControlSystem:
@@ -67,7 +83,7 @@ class ControlSystem:
 
         # prevent data loss
         atexit.register(self.exit)
-        self.heartbeat_file = os.path.join(os.path.dirname(__file__), "heartbeat.json")
+        self.heartbeat_file = os.path.join(os.path.dirname(__file__), ".heartbeat.json")
 
         # Check for unclean shutdown
         if self.was_unclean_shutdown():
@@ -143,19 +159,19 @@ class App:
 
         # create frames
         self.root_div1 = QWidget(self.root)
-        self.root_div1.setStyleSheet("background-color: #f5f5dc")
+        self.root_div1.setStyleSheet(f"background-color: {background_color_light};")
         self.root_div1.setFixedSize(div_size, div_size) # 224 x 224
 
         self.root_div2 = QWidget(self.root)
-        self.root_div2.setStyleSheet("background-color: #f5f5dc")
+        self.root_div2.setStyleSheet(f"background-color: {background_color_light};")
         self.root_div2.setFixedSize(div_size - pad, int(div_size * 0.25)) # 224 x 56
 
         self.root_div3 = QWidget(self.root)
-        self.root_div3.setStyleSheet("background-color: #f5f5dc")
+        self.root_div3.setStyleSheet(f"background-color: {background_color_light};")
         self.root_div3.setFixedSize(div_size - pad, int(div_size * 0.75) - pad) # 224 x 168
 
         self.root_div4 = QWidget(self.root)
-        self.root_div4.setStyleSheet("background-color: #f5f5dc")
+        self.root_div4.setStyleSheet(f"background-color: {background_color_light};")
         self.root_div4.setFixedSize(div_size * 2, int(div_size * 0.25)) # 448 x 56
 
         # position frames
@@ -178,31 +194,33 @@ class App:
         # validator
         self.validator_label = QLabel(self.root_div2)
         self.validator_label.setText(f"Validator: {self.control.current_validator()}")
-        self.validator_label.setStyleSheet("background-color: #f5f5dc; color: black;")
+        self.validator_label.setStyleSheet(f"background-color: {background_color_light}; color: {text_color};")
+        self.validator_label.setFixedSize(int(div_size) - pad * 3, int(div_size * 0.09375)) # 168 x 21
 
         # dropdown menu
         self.validator_dropdown = QtWidgets.QComboBox(self.root_div2)
-        self.validator_dropdown.setStyleSheet(""" 
-            QComboBox {
-                border: 1px solid #ced4da;
+        self.validator_dropdown.setStyleSheet(f""" 
+            QComboBox {{
+                border: 1px solid {background_color_dark};
                 border-radius: 4px;
                 padding-left: 10px;
-            }
-            QComboBox::drop-down {
+                color: {text_color};
+            }}
+            QComboBox::drop-down {{
                 border: 0px;
-            }
-            QComboBox::down-arrow {
+            }}
+            QComboBox::down-arrow {{
                 image: url(./app/system_img/dropdown_arrow.png);
                 width: 12px;
                 height: 12px;
                 margin-right: 15px;
-            }
-            QComboBox:on {
-                border: 2px solid #f1e1ff;
-            }
-            QComboBox::item:selected {
-                color: #b177ff;
-            }
+            }}
+            QComboBox:on {{
+                border: 2px solid {text_color};
+            }}
+            QComboBox::item:selected {{
+                color: {background_color_dark};
+            }}
             """
         )
         self.validator_dropdown.setFixedSize(int(div_size * 0.75), int(div_size * 0.09375)) # 168 x 21
@@ -217,12 +235,14 @@ class App:
         # current label text
         self.current_label_text = QLabel(self.root_div3)
         self.current_label_text.setText("Current Labels:")
-        self.current_label_text.setStyleSheet("background-color: #f5f5dc; color: black;")
+        self.current_label_text.setStyleSheet(f"background-color: {background_color_light}; color: {text_color};")
+        self.current_label_text.setFixedSize(int(div_size) - pad * 3, int(div_size * 0.09375)) # 168 x 21
 
         # current labels
         self.current_label_list = QtWidgets.QListWidget(self.root_div3)
         self.current_label_list.addItems(self.control.current_labels())
-        self.current_label_list.setFixedSize(int(div_size) - pad * 2, int(div_size * 0.5)) # 168 x 147
+        self.current_label_list.setFixedSize(int(div_size) - pad * 3, int(div_size * 0.75) - pad * 4 - int(div_size * 0.09375)) # 168 x 147
+        self.current_label_list.setStyleSheet(f"color: {text_color};")
 
         # position items in root_div3
         self.current_label_text.move(pad, pad)
@@ -232,31 +252,31 @@ class App:
         # buttons
         self.next_image_button = QtWidgets.QPushButton(self.root_div4)
         self.next_image_button.setText("Next Image")
-        self.next_image_button.setStyleSheet("background-color: white")
+        self.next_image_button.setStyleSheet(button_style)
         self.next_image_button.setFixedSize(int(div_size * 0.5), int(div_size * 0.09375)) # 112 x 21
         self.next_image_button.clicked.connect(self.next_image)
 
         self.previous_image_button = QtWidgets.QPushButton(self.root_div4)
         self.previous_image_button.setText("Previous Image")
-        self.previous_image_button.setStyleSheet("background-color: white")
+        self.previous_image_button.setStyleSheet(button_style)
         self.previous_image_button.setFixedSize(int(div_size * 0.5), int(div_size * 0.09375)) # 112 x 21
         self.previous_image_button.clicked.connect(self.previous_image)
 
         self.accept_button = QtWidgets.QPushButton(self.root_div4)
         self.accept_button.setText("Accept")
-        self.accept_button.setStyleSheet("background-color: white")
+        self.accept_button.setStyleSheet(button_style)
         self.accept_button.setFixedSize(int(div_size * 0.5), int(div_size * 0.09375)) # 112 x 21
         self.accept_button.clicked.connect(lambda: self.record_result("accept"))
 
         self.incorrect_button = QtWidgets.QPushButton(self.root_div4)
         self.incorrect_button.setText("Incorrect")
-        self.incorrect_button.setStyleSheet("background-color: white")
+        self.incorrect_button.setStyleSheet(button_style)
         self.incorrect_button.setFixedSize(int(div_size * 0.5), int(div_size * 0.09375)) # 112 x 21
         self.incorrect_button.clicked.connect(lambda: self.record_result("incorrect"))
 
         self.reject_button = QtWidgets.QPushButton(self.root_div4)
         self.reject_button.setText("Reject")
-        self.reject_button.setStyleSheet("background-color: white")
+        self.reject_button.setStyleSheet(button_style)
         self.reject_button.setFixedSize(int(div_size * 0.5), int(div_size * 0.09375)) # 112 x 21
         self.reject_button.clicked.connect(lambda: self.record_result("reject"))
 
